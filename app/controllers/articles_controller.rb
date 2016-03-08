128a1c5acf2d7e5481c95750704509781f114bcd
Matthew J. Smith <msmith8504@gmail.com>
2016-03-08 12:30:43 -0500

    Added partial (for view, and edit) and delete action.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 	
+	def index
+		@articles = Article.all
+	end
+	
 	def new
 		@article = Article.new
 	end
@@ -16,6 +20,27 @@ class ArticlesController < ApplicationController
 	
 	def show
 		@article = Article.find(params[:id])
+	end
+    
+    def destroy 
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article was deleted."
+        redirect_to articles_path
+    end
+	
+	def edit
+		@article = Article.find(params[:id])
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Success! Article was updated."
+			redirect_to article_path(@article)
+		else
+			render :edit
+		end
 	end
 	
 	private
